a63e1080f7efa4f6364843c90b592ffb0d0cc0fc
darcierre <darrnurse@gmail.com>
2025-01-10 01:02:37 -0500

    Added date sort and icon animation. Changed focus styles.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,10 @@ export default function Home() : ReactNode {
     const [photos, setPhotos] = useState<IBasePhoto[]>([]);
     const [searchValue, setSearchValue] = useState<string>("");
     const [searchVisible, setSearchVisible] = useState<boolean>(false);
-    const [searchClickedOnce, setSearchClickedOnce] = useState(false); // this needs to burn
-    const [fetchSuccess, setFetchSuccess] = useState(true);
+    const [searchClickedOnce, setSearchClickedOnce] = useState<boolean>(false); // this needs to burn
+    const [fetchSuccess, setFetchSuccess] = useState<boolean>(true);
+    const [sortNewest, setSortNewest] = useState<boolean>(true);
+    const [dateSortedOnce, setDateSortedOnce] = useState<boolean>(false); // combust
 
     const serverUrl = import.meta.env.VITE_SERVER_URL;
     const homeUrl = serverUrl + "/photos";
@@ -65,7 +67,7 @@ export default function Home() : ReactNode {
             <div className={"p-8 relative grid grid-cols-3 z-[1] w-full"}>
                 <div className={"flex items-center"}>
                     <button
-                        className={"outline-white p-1"}
+                        className={"outline-white sort-button rounded-[50%] p-3"}
                         tabIndex={0}
                         onClick={() => {
                             setSearchVisible(!searchVisible)
@@ -78,29 +80,28 @@ export default function Home() : ReactNode {
                         <LazyLoadImage
                             src={"/search.svg"}
                             className={`${searchClickedOnce ? (searchVisible ? "search-spin-left" : "search-spin-right") : ""} h-[2rem] cursor-pointer hover:scale-[105%] active:scale-[95%]`}
-                            alt={"clear-search"}
-
+                            alt={"search-image"}
                         />
                     </button>
-
                 </div>
+
                 <div
-                    className={`${searchVisible ? "" : "pointer-events-none"} ${searchVisible ? "search-box-left" : "search-box-right"} absolute top-[2rem] flex gap-3 items-center`}
+                    className={`${searchVisible ? "" : "pointer-events-none"} ${searchVisible ? "search-box-left" : "search-box-right"} absolute top-[2.5rem] flex gap-3 items-center`}
                     style={{
                         opacity: searchVisible ? 1 : 0,
                         transition: "opacity ease 0.75s"
-                }}
+                    }}
                 >
                     <input
                         value={searchValue}
                         onChange={handleSearch}
-                        className={"p-2 rounded-lg"}
+                        className={"p-2 rounded-lg sort-button outline-white"}
                         ref={searchRef}
                         tabIndex={searchVisible ? 0 : -1}
                     />
                     <button
                         onClick={() => setSearchValue("")}
-                        className={"cursor-pointer outline-white p-1 hover:scale-[105%] active:scale-[95%]"}
+                        className={"cursor-pointer sort-button rounded-[50%] outline-white p-2 hover:scale-[105%] active:scale-[95%]"}
                         tabIndex={searchVisible ? 0 : -1}
                     >
                         <img
@@ -111,20 +112,39 @@ export default function Home() : ReactNode {
                     </button>
                 </div>
 
+                <div className={"col-start-3 flex p-2 justify-end"}>
+                    <button
+                        className={"outline-white sort-button p-2 flex justify-center relative items-center border-[0.3rem] rounded-[50%] aspect-square border-white"}
+                        tabIndex={0}
+                        onClick={() => {
+                            setDateSortedOnce(true);
+                            setSortNewest(!sortNewest);
+                        }}
+                        type={"button"}
+                    >
+                        <LazyLoadImage
+                            src={"/pin.svg"}
+                            className={`${dateSortedOnce ? (sortNewest ? "search-spin-left" : "search-spin-right") : ""} relative top-[0.5rem] pin h-[1rem] cursor-pointer hover:scale-[105%] active:scale-[95%]`}
+                            alt={"change-date"}
+                        />
+                    </button>
+                </div>
+
             </div>
-            <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 p-[5rem] pt-[1rem] gap-6 gap-y-12 relative z-[1]"}>
+            <div
+                className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 p-[5rem] pt-[1rem] gap-6 gap-y-12 relative z-[1]"}>
                 {photos && (photos.length >= 1 ?
                         photos
                             .filter((photo) => photo.title.toLowerCase().includes(searchValue))
                             .sort((photo1, photo2) => {
-                                return new Date(photo2.date_taken) - new Date(photo1.date_taken)
-                                // implement toggle sort, find good icons
+                                if(sortNewest) return new Date(photo2.date_taken) - new Date(photo1.date_taken);
+                                else return new Date(photo1.date_taken) - new Date(photo2.date_taken);
                             })
                             .map((photo, index) => (
-                                    <PhotoCard
-                                        url={photo.url}
-                                        title={photo.title}
-                                        date_taken={photo.date_taken}
+                                <PhotoCard
+                                    url={photo.url}
+                                    title={photo.title}
+                                    date_taken={photo.date_taken}
                                         location={photo.location}
                                         slug={photo.slug}
                                         key={index}
