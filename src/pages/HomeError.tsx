1a519fb28f8562d4e4d7b10f49f787b74d0715bb
darcierre <darrnurse@gmail.com>
2025-02-04 00:18:03 -0500

    Testing ios fix.

--- src/pages/HomeError.tsx
+++ src/pages/HomeError.tsx
@@ -1,6 +1,6 @@
 export default function HomeError() {
     return (
-        <div className={"h-[85vh] flex w-full justify-center items-center text-white bg-black font-bold text-2xl text-center"}>
+        <div className={"h-[85vh] flex w-full justify-center items-center text-white bg-black p-4 font-bold text-2xl text-center"}>
             It seems like no photos were taken!
         </div>
     )
